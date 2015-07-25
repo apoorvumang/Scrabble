fadf7cac8ee295671a49a38dad1a9ac7561e59ee
Aakash Chugh <akshchugh.ac@gmail.com>
2015-07-25 08:47:04 +0530

    Create PrinterUtilityTest.java

---
+++ test/PrinterUtilityTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PrinterUtilityTest {
+
+	@Test
+	public void somefunction() { //Test the PrintWords function for code coverage
+	
+	}
+
+}
